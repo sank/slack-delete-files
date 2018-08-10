afc98361f859de705257bdf940d9b0bf7ad1f8d8
Sank <as@subtotal.ru>
2018-08-10 11:40:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Delete all files from slack
+
+You can take token here: https://api.slack.com/custom-integrations/legacy-tokens 
---
+++ draft.py
@@ -0,0 +1,35 @@
+import requests
+import datetime
+import time
+import calendar
+
+def date_to_unixtime(dt):
+    """
+    Convert python datetime to Unix timestamp
+    :param dt: python datetime
+    :return: Unix time
+    """
+    return calendar.timegm(d.timetuple())
+
+token='xoxp-16568988338-17559283952-414933461047-2b21fb33c6df09d614f00024d9d73f76'
+url = 'https://slack.com/api/files.list'
+url_del = 'https://slack.com/api/files.delete'
+dt = int(date_to_unixtime(datetime.datetime(2018, 7, 3, 16, 23, 29)) / 1000000)
+r = requests.get(url, params={'token': token, 'ts_to': dt})
+last_page = r.json()['paging']['pages']
+
+for i in range(last_page):
+    r = requests.get(url, params={'token': token, 'page': i+1, 'ts_to': dt})
+    if r.status_code != 200:
+        print('Error: %s' % r.text)
+        break
+    else:
+        f = r.json()
+        for f in f['files']:
+            r = requests.post(url_del, params={'token': token, 'file': f['id']})
+            if r.status_code != 200:
+                print('Error: %s' % r.text)
+                break
+        print('page %s ok' % (i+1))
+    time.sleep(60)
+
